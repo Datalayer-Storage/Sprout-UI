c92d1d9edd9844fbc24e56b4464527b9b4e92a15
Michael Taylor <mtaylor@michaeltaylor.dev>
2024-01-04 10:40:59 -0500

    feat: set up project

---
+++ src/main.ts
@@ -0,0 +1,38 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  if (process.env.NODE_ENV === "development") {
+    win.loadURL("http://localhost:5173/");
+  } else {
+    win.loadFile("dist/index.html");
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
