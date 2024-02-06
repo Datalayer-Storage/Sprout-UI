50448aa30092f54c6548aa2a5aef76bc96ba0430
wwills2 <wwills2@umbc.edu>
2024-02-06 15:57:09 -0500

    chore: corrected relative inputs

--- src/renderer/components/blocks/layout/index.tsx
+++ src/renderer/components/blocks/layout/index.tsx
@@ -2,5 +2,4 @@ export * from './LeftNav';
 export * from './Template';
 export * from './WebView';
 export * from './SelectedStoreIdCard';
-export * from '../../form/settings';
 export * from './XTerm';
--- src/renderer/components/form/index.ts
+++ src/renderer/components/form/index.ts
@@ -1,1 +1,2 @@
 export * from './FolderSelector';
+export * from './settings';
