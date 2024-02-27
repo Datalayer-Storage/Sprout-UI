0be95e1e7946f8f03f1e344bb6c6f44ea3876f6b
wwills2 <wwills2@umbc.edu>
2024-02-27 17:23:13 -0500

    chore: removed modal header component

--- src/renderer/components/blocks/layout/ModalHeaderNoClose.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const ModalHeaderNoClose: React.FC<React.HtmlHTMLAttributes<HTMLDivElement>> = ({...props}) => {
-  return (
-    <div className={'flex items-start justify-between rounded-t dark:border-gray-600 border-b p-5'} {...props}/>
-  );
-}
-
-export {ModalHeaderNoClose}
